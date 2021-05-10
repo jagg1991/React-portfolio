38622fe18404d9752ce5db2a649bb223c1670dbd
jagg1991 <josephguajardo@ymail.com>
2021-05-10 14:36:41 -0700

    adding navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+
+function Navbar() {
+    return (
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+
+
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">Portfolio</a>
+                        <a className="nav-link" href="#">About</a>
+                        <a className="nav-link" href="#">Contact</a>
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    )
+
+}
+
+export default Navbar;
